168aebcadec3d69a5f29705fa7eb4beeb96b9955
pocipooh <88044107+pocipooh@users.noreply.github.com>
2021-12-13 23:48:54 +0700

    Update bannerkiri.js
    change new

--- bannerkiri.js
+++ bannerkiri.js
@@ -7,7 +7,7 @@ div.innerHTML = `
   <button style="z-index: 999920;position: absolute;float: left;top: 0px;right: 0px;width: 30px;cursor: pointer;height: 30px;background-repeat: no-repeat;background-size: cover;background-color: #fdd736; color:#202020;" alt="close" title="Close Ads">X </button>
 </a>
 <a href="https://174.138.30.135/" title="MBO128">
-  <img style="width:200px;height:auto;" src="https://1.bp.blogspot.com/-OpjUMjNcak0/YOVfJcLmhWI/AAAAAAAAGkY/oMNcfREZQ3YskPDz2fq2DcGIQe1ax0e1wCLcBGAsYHQ/s0/SITUS-JUDI-ONLINE-DEPOSIT-PULSA.gif" alt="MBO128">
+  <img style="width:200px;height:auto;" src="https://www.demoslotonline.net/wp-content/uploads/2021/08/BVGAMING_banner.gif" alt="MBO128">
 </a>
 </span>
 <span class="left-banner-hideshow" id="left_banner2" style="top:750px; left:40px; position:fixed; z-index:100; overflow:hidden; clear:right;">
